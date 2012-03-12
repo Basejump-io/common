acb51b8d31ce7869231562c89295f606ce911b2b
Tony Wallace <awallace@quicinc.com>
2012-03-12 14:22:54 -0700

    ws

--- unit_test/cmtest.cc
+++ unit_test/cmtest.cc
@@ -20,8 +20,8 @@
 int main(int argc, char**argv, char**envArg)
 {
     int status = 0;
-    setvbuf(stdout,NULL,_IONBF,0);
-    setvbuf(stderr,NULL,_IONBF,0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
 
     printf("\n Running common unit test \n");
     testing::InitGoogleTest(&argc, argv);
