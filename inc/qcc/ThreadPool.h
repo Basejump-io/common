d714d282abe60ccf2d19055a15c925225bbf31fc
Craig Dowell <cdowell@quicinc.com>
2012-04-25 10:58:12 -0700

    Add smart pointer class, break ThreadPool into header and impl
    Change-Id: I8980c9ebf73f3cf31aea0b376cdc3795d5973d14

--- inc/qcc/ThreadPool.h
+++ inc/qcc/ThreadPool.h
@@ -23,36 +23,62 @@
 #ifndef _QCC_THREADPOOL_H
 #define _QCC_THREADPOOL_H
 
+#include <map>
+
 #include <qcc/Timer.h>
+#include <qcc/Event.h>
+#include <qcc/Ptr.h>
 
 namespace qcc {
 
+class ThreadPool;
+
 /**
- * A class in the spirit of the Java Runnable object that is used to define
- * an object which is executable by a ThreadPool.
+ * A class in the spirit of the Java Runnable object that is used to define an
+ * object which is executable by a ThreadPool.
  *
- * In order to ask the ThreadPool to execute a task, inherit from the
- * Runnable class and provide a Run() method.  You must allocate a Runnable
- * object on the heap and then call ThreadPool::Execute() providing a pointer
- * to the Runnable object.
+ * In order to ask the ThreadPool to execute a task, inherit from the Runnable
+ * class and provide a Run() method.
  *
- * Once the Run() method has executed, the runnable will be automatically
- * deleted.
+ * Typically one extends the Runnable object with member data corresponding to
+ * the deferred variables needed by the Run() method.  This kind of object is
+ * called a closure.  Since the data must be kept outside of the context of the
+ * function or method that asks for execution of the Runnable, it must be kept
+ * on the heap.
+ *
+ * The AllJoyn common OS abstractions library provides a ManagedObj (similar to
+ * smart pointers but using references) to help with this kind of need, but it
+ * has problems if you want to cast from one kind of ManagedObj to another (see
+ * the single parameter constructor and explicit call to the destructor of the
+ * enclosed type.  Since we need the cast behavior, we use the Ptr intrusive
+ * smart pointer class to manage our runnable closures.
  */
-class Runnable : private qcc::AlarmListener {
+class Runnable : public qcc::RefCountBase, public qcc::AlarmListener {
   public:
+
+    /**
+     * Construct a Runnable object suitable for use by a ThreadPool
+     */
+    Runnable() : m_threadpool(NULL) { }
+
+    /**
+     * Destroy a Runnable object.
+     */
+    virtual ~Runnable() { m_threadpool = NULL; }
+
     /**
      * This method is called by the ThreadPool when the Runnable object is
      * dispatched to a thread.  A client of the thread pool is expected to
-     * define a method in a derived class that does useful work when Run()
-     * is called.
+     * define a method in a derived class that does useful work when Run() is
+     * called.
      */
-    virtual void Run(void) = 0;
+    virtual void Run(void) { };
 
   private:
     /**
-     * ThreadPool must be a friend in order to make AlarmTriggered an
-     * accessible base class method of a user's Runnable.
+     * ThreadPool must be a friend in order to make AlarmTriggered an accessible
+     * base class method of a user's Runnable, and to allow the ThreadPool to
+     * set a pointer to itself here to allow for notifications.
      */
     friend class ThreadPool;
 
@@ -64,11 +90,25 @@ class Runnable : private qcc::AlarmListener {
      * Note that the enclosing runnable object is automatically deleted
      * afte the Run method is executed.
      */
-    virtual void AlarmTriggered(const Alarm& alarm, QStatus reason)
+    virtual void AlarmTriggered(const Alarm& alarm, QStatus reason);
+
+    /**
+     * Private method used by the thread pool to tell this object how to contact
+     * the thread pool when the reference to the runnable closure is no longer
+     * needed.
+     */
+    void SetThreadPool(ThreadPool* threadpool)
     {
-        Run();
-        delete this;
+        m_threadpool = threadpool;
     }
+
+    /**
+     * A reference back to the thread pool that causes the run method to be
+     * called.  We need this reference so the thread pool can hook a "done"
+     * event since it would otherwise be unaware of the execution of the 
+     * runderlying timer alarm callback that it arranged to be called.
+     */
+    ThreadPool* m_threadpool;
 };
 
 /**
@@ -85,32 +125,19 @@ class ThreadPool {
      * Construct a thread pool with a given name and pool size.
      *
      * @param name     The name of the thread pool (used in logging).
-     * @param poolSize The number of threads available in the pool.
+     * @param poolsize The number of threads available in the pool.
      */
-    ThreadPool(const char* name, uint32_t poolSize) : dispatcher(name, false, poolSize)
-    {
-        /*
-         * Start the dispatcher timer.  This timer will have a concurrency
-         * corresponding to the poolSize provided.  This means that there will
-         * be <poolSize> theads waiting to dispatch expired alarms.  The
-         * concurrent threads in the Timer provide us with our thread pool.
-         */
-        dispatcher.Start();
-    }
+    ThreadPool(const char* name, uint32_t poolsize);
 
     /**
      * Destroy a thread pool.
      */
-    virtual ~ThreadPool()
-    {
-        Stop();
-        Join();
-    }
+    virtual ~ThreadPool();
 
     /**
-     * Requesting the we cancel all of our dispatched threads.
+     * Request that we cancel all of our dispatched threads.
      */
-    QStatus Stop() { return dispatcher.Stop(); }
+    QStatus Stop() { return m_dispatcher.Stop(); }
 
     /**
      * Wait for all of the threads in our associated timer to exit.  Once
@@ -118,7 +145,34 @@ class ThreadPool {
      * Note that this call can block or a time limited only by the execution
      * time of the threads dispatched.
      */
-    QStatus Join() { return dispatcher.Join(); }
+    QStatus Join() { return m_dispatcher.Join(); }
+
+    /**
+     * Determine the underlying concurrency of the thread pool.
+     *
+     * Convenience function to get the number of threads available in the thread pool.
+     * This is different than GetN() which returns the number of threads that are
+     * currently executing or waiting to execute a closure.
+     *
+     * @return The number of concurrently execution tasks possible in this thread pool.
+     */
+    uint32_t GetConcurrency(void)
+    {
+        return m_poolsize;
+    }
+
+    /**
+     * Determine how many Runnable tasks are pending on the thread pool.
+     *
+     * Convenience function to get the number of threads currently executing or
+     * waiting to execute.  This is different than GetConcurrency() which
+     * returns the number of threads in the thread pool (which may or may not be
+     * currently executing or waiting to execute.
+     *
+     * @return The number of Runnable closures which represent either pending
+     *         execution or are currently executing runnable tasks.
+     */
+    uint32_t GetN(void);
 
     /**
      * Execute a Runnable task one one of the threads of the thread pool.
@@ -134,24 +188,45 @@ class ThreadPool {
      *
      * Each call to Execute must provide a pointer to a unique Runnable
      *
-     * @param runnable A pointer to a Runnable object providing the Run() method
-     *                 which one of the threads in this thread pool will execute.
+     * @param runnable A Ptr (smart pointer) to a Runnable Object providing the
+     *                 Run() method which one of the threads in this thread pool
+     *                 will execute.
+     *
+     * @return
+     *      - #ER_OK if the execute request was successful
+     *      - #ER_THREADPOOL_EXHAUSTED if the thread pool has previously reached its specified concurrency.
      */
-    void Execute(Runnable* runnable)
-    {
-        /*
-         * The trick here is to add an alarm that expires immediately and
-         * executes the AlarmTriggered method of the provided Runnable
-         * object.  This will call the Run() method of the Runnable.  So
-         * although we use Timers and Alarms, we schedule everthing to
-         * happen immediately, and the result looks like a thread pool
-         * that we all know and love.
-         */
-        qcc::Alarm alarm = qcc::Alarm(0, runnable, 0, NULL);
-        dispatcher.AddAlarm(alarm);
-    }
+    QStatus Execute(Ptr<Runnable> runnable);
+
+    /**
+     * Wait for a thread to become available for use.
+     *
+     * Since AllJoyn is at its heart a distributed network application, and what
+     * drives the execution of our threads will ultimately be network traffic,
+     * we need to be able to apply backpressure to the network to avoid
+     * exhausting all available resources.  We do this by providing a method which
+     * allows a caller to put itself to sleep until a thread beomes available.
+     *
+     * If the caller thread happens to be the thread that is reading messages off
+     * of the network, it will put itself to sleep.  This will stop the network
+     * destination from pulling bits out of its queue and will result in, for example,
+     * the TCP receive queue filling.  This will, in turn, apply backpressure to the
+     * sender which will rate limit the system.  This prevents unbounded resource
+     * allocation of corresponding threads and memory.
+     *
+     * @return ER_OK when a thread becomes available, or a general error if one happens.
+     */
+    QStatus WaitForAvailableThread(void);
 
   private:
+    /**
+     * Runnable must be a friend in order to call back and tell us that it has
+     * finished executing.  Since this affects the memory management contract
+     * between the Runnable and ThreadPool objects, we don't let anyone else
+     * but Runnable make this call.
+     */
+    friend class Runnable;
+
     /**
      * Assignment operator is private - ThreadPools cannot be assigned.
      */
@@ -162,7 +237,48 @@ class ThreadPool {
      */
     ThreadPool(const ThreadPool& other);
 
-    qcc::Timer dispatcher;
+    /**
+     * A mutex to protect the underlying data structures.
+     */
+    qcc::Mutex m_lock;
+
+    /**
+     * An event to allow callers to wait until a thread becomes available
+     * in the pool.
+     */
+    qcc::Event m_event;
+
+    /**
+     * The maximum number of concurrent threads executing in this thread pool.
+     */
+    uint32_t m_poolsize;
+
+    /**
+     * We need to hold a reference to an underlying Runnable object while we
+     * wait for its Run() method to be executed.  The Runnable object acts as a
+     * closure that a user will use to keep required variables.  When the Run()
+     * method is done executing, we release the reference to the runnable managed
+     * object by deleting the corresponding entry in the closure map.
+     */
+    typedef std::map<Runnable*, Ptr<Runnable> > RunnableEntry;
+    RunnableEntry m_closures;
+
+    /**
+     * When the AlarmTriggered callback of the AlarmListener base class of the
+     * Runnable is called by the Timer, it will call the user's Run() function
+     * and then call back here to let us know that it has finished executing.
+     * That tells us that we can clean up any resources we may be holding.  That
+     * just means releasing our reference to the Ptr that holds the ulitmate
+     * reference to the runnable.  If we can't find a closure, we have screwed
+     * up massively and we should just die.
+     */
+    void Release(Runnable* runnable);
+
+    /**
+     * The timer that actually does all of the work of dispatching threads and
+     * essentially doing all of the work of the thread pool.
+     */
+    qcc::Timer m_dispatcher;
 };
 
 } // namespace qcc
---
+++ src/ThreadPool.cc
@@ -0,0 +1,267 @@
+/**
+ * @file
+ *
+ * Simple ThreadPool using Timers and Alarms
+ */
+
+/******************************************************************************
+ * Copyright 2012, Qualcomm Innovation Center, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ ******************************************************************************/
+
+#include <assert.h>
+#include <qcc/ThreadPool.h>
+
+#define QCC_MODULE "THREADPOOL"
+
+namespace qcc {
+
+void Runnable::AlarmTriggered(const Alarm& alarm, QStatus reason)
+{
+    QCC_DbgPrintf(("Runnable::AlarmTriggered()"));
+
+    /*
+     * Execute the user's provided run function.  Note that this is done in an
+     * OS-specific context.  For example, on Linux-based systems, this may
+     * happen in the context of a pthread from the Timer; but on Windows systems
+     * it may happen in the context of an asynchronous OS callback.
+     */
+    Run();
+
+    /*
+     * Tell the threadpool that we are done with this runnable object.  This
+     * allows the threadpool to release its reference to the object which may
+     * result in an immediate delete of the object.  So one must never refer to
+     * the underlying runnable after this point.
+     */
+    m_threadpool->Release(this);
+}
+
+ThreadPool::ThreadPool(const char* name, uint32_t poolsize)
+    : m_poolsize(poolsize), m_dispatcher(name, false, poolsize)
+{
+    QCC_DbgPrintf(("ThreadPool::ThreadPool()"));
+
+    assert(poolsize && "ThreadPool::ThreadPool(): Empty pools are no good for anyone");
+
+    /*
+     * Start the dispatcher Timer.  The Timer is the code that will cause alarms
+     * to be executed.  It is the alarms that provide the callbacks that the
+     * timer will fire.  Our timer will have a concurrency variable that
+     * corresponds to the poolsize provided.  This means that there will be
+     * <poolsize> theads waiting to dispatch expired alarms (either AllJoyn
+     * created in Posix systems or OS dispatched in Windows systems).  The
+     * concurrent threads in the Timer provide us with the thread pool we
+     * actually use.
+     */
+    m_dispatcher.Start();
+
+    /*
+     * Set the event that callers will ultimately use to sleep on until a thread
+     * becomes available.  We just created a Timer dispatcher which has
+     * concurrency of at least one, so there is definitely a thread available.
+     */
+    m_event.SetEvent();
+}
+
+ThreadPool::~ThreadPool()
+{
+    QCC_DbgPrintf(("ThreadPool::~ThreadPool()"));
+
+    Stop();
+    Join();
+}
+
+/*
+ * Convenience function to get the number of threads currently executing or
+ * waiting to execute.  This is different than GetConcurrency() which returns
+ * the number of threads in the thread pool (which may or may not be currently
+ * executing or waiting to execute.
+ */
+uint32_t ThreadPool::GetN(void)
+{
+    QCC_DbgPrintf(("ThreadPool::GetN()"));
+
+    uint32_t n = 0;
+    m_lock.Lock();
+    n = m_closures.size();
+    m_lock.Unlock();
+    return n;
+}
+
+QStatus ThreadPool::Execute(Ptr<Runnable> runnable)
+{
+    QCC_DbgPrintf(("ThreadPool::Execute()"));
+
+    m_lock.Lock();
+
+    /*
+     * Since AllJoyn is at its heart a distributed network application, and what
+     * drives the execution of our threads will be network traffic, we need to
+     * be able to apply backpressure to the network to avoid exhausting all
+     * available resources.  This is enabled by returning an error when all of
+     * the threads are in process.  This is a thread pool, not a work queue.
+     */
+    if (m_closures.size() == m_poolsize) {
+        m_lock.Unlock();
+        QCC_DbgPrintf(("ThreadPool::Execute(): Exhausted"));
+        return ER_THREADPOOL_EXHAUSTED;
+    }
+
+    /*
+     * We need to make sure that the runnable object is kept alive while it is
+     * waiting to be run (and while it is running) so we keep a reference to it
+     * until we don't need it any more.  This also allows us to keep track of
+     * how many pending operations there are.  Note that the map is keyed on the
+     * underlying runnable object pointer, but the smart pointer is saved in the
+     * map in order to hold a reference to the object.
+     */
+    m_closures[runnable.Peek()] = runnable;
+    m_lock.Unlock();
+
+    /*
+     * Tell the runnable object where to contact us when it is done executing.
+     * Since we're not the one that is actually going to dispatch the thread
+     * we need to have whatever thread that goes in there call us us back so
+     * we can know when to release the object.
+     */
+    runnable->SetThreadPool(this);
+
+    /*
+     * The trick here is to add an alarm that expires immediately and executes
+     * the AlarmTriggered method of the provided Runnable object.  This will
+     * call the Run() method of the Runnable.  So although we use Timers and
+     * Alarms, we schedule everthing to happen immediately, and the result looks
+     * like a thread pool that we all know and love.
+     *
+     * Since <runnable> is a smart pointer, we need to get the underlying object
+     * pointer using Peek().  This object pointer is automagically cast to an
+     * AlarmListener which defines which actual callback which the alarm will
+     * call.
+     */
+    Alarm alarm = Alarm(0, runnable.Peek(), 0, NULL);
+    QCC_DbgPrintf(("ThreadPool::Execute(): AddAlarm()"));
+    return m_dispatcher.AddAlarm(alarm);
+}
+
+void ThreadPool::Release(Runnable* runnable)
+{
+    QCC_DbgPrintf(("ThreadPool::Release()"));
+
+    /*
+     * After the closure corresponding to the Runnable pointer above has finished
+     * executing, it needs to call us back here and tell us that its heap object is
+     * no longer required; and that we can release our hold on that object.
+     *
+     * We save a smart pointer to the Runnable in our m_closures map, so we need to
+     * erase that entry.  Since the entry is a smart pointer, the act of erasing it
+     * will cause the reference count of the object to be decremented and the
+     * underlying object to be deleted if possible.
+     */
+    m_lock.Lock();
+    RunnableEntry::iterator i = m_closures.find(runnable);
+    assert(i != m_closures.end() && "ThreadPool::Release(): Cannot find closure to release");
+    m_closures.erase(i);
+
+    /*
+     * Release needs to work in conjunction with Execute() and
+     * WaitForAvailableThread() to ensure that no than m_poolSize threads are
+     * dispatched at any one time.  We set an event every time a thread
+     * completes its Run() method which can then wake up an external thread
+     * waiting for an available thread to do its work.
+     */
+    m_event.SetEvent();
+    m_lock.Unlock();
+}
+
+QStatus ThreadPool::WaitForAvailableThread(void)
+{
+    QCC_DbgPrintf(("ThreadPool::WaitForAvailableThread()"));
+
+    /*
+     * This method needs to work in conjunction with Execute() and Releas() to
+     * ensure that no more than m_poolSize threads are dispatched at any one
+     * time.
+     */
+    m_lock.Lock();
+    bool threadAvailable = m_closures.size() < m_poolsize;
+
+    /*
+     * There is a closure on the m_closures map for the duration of time when a
+     * Runnable closure is waiting to be executed and in process.  So, if the
+     * size of the m_closures map is less than the pool size, it means that not
+     * all of our thread pool threads are occupied and at least one is
+     * available, so we just return.
+     */
+    if (threadAvailable) {
+        m_lock.Unlock();
+        QCC_DbgPrintf(("ThreadPool::WaitForAvailableThread(): Thread available"));
+        return ER_OK;
+    }
+
+    /*
+     * We know that all of our threads are occupied, so we have to wait until
+     * one of the Runnable closures is executed.  At the end of the process,
+     * Release() will be called.  This will remove a closure from the m_closures
+     * map and set m_event.  Our job here is then to wait until the event is
+     * set and then look around to see if we should return.  We should only
+     * return if there's a thread available to avoid the thundering herd.
+     */
+    for (;;) {
+        m_lock.Unlock();
+        QCC_DbgPrintf(("ThreadPool::WaitForAvailableThread(): Waiting on thread completion event"));
+
+        /*
+         * We are executing in the context of some unknown (to us) thread.  This
+         * thread can be stopped and alerted using its own mechanisms so we have
+         * to play fair with all of that.  The only return code we are interested
+         * in is ER_OK, which means that we set our event.  Any other error code
+         * should be returned to the caller, who can figure out the right thing
+         * to do.
+         */
+        QStatus status = Event::Wait(m_event, Event::WAIT_FOREVER);
+        if (status != ER_OK) {
+            QCC_DbgPrintf(("ThreadPool::WaitForAvailableThread(): Event::Wait() error"));
+            return status;
+        }
+
+        QCC_DbgPrintf(("ThreadPool::WaitForAvailableThread(): Got thread completion event"));
+        m_lock.Lock();
+
+        /*
+         * Only allow one thead to return per Release() (thread completion)
+         * event.
+         */
+        m_event.ResetEvent();
+
+        /*
+         * If there's an available thread, return and allow the calling thread
+         * to proceed with its presumable Execute().  If there's mo available
+         * thread, we need to Event::Wait() again and wait for one.  Since
+         * there's one event, it is possible that multiple waiting threads are
+         * awakened when a thread completes.
+         */
+        if (m_closures.size() < m_poolsize) {
+            m_lock.Unlock();
+            QCC_DbgPrintf(("ThreadPool::WaitForAvailableThread(): Thread available"));
+            return ER_OK;
+        }
+    }
+
+    m_lock.Unlock();
+    assert(false && "ThreadPool::WaitForAvailableThread(): Can't happen");
+    return ER_FAIL;
+}
+
+} // namespace qcc
