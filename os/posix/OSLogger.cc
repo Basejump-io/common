8d8daa1aae3cea406c2db5a7d83e5edbbda6cb41
Todd Malsbary <tmalsbar@quicinc.com>
2012-08-09 11:13:18 -0700

    Fix OS logging for android.
    Change-Id: I4b53f300aacaeeab5acaa116b670e3bbaca7bb74

--- os/posix/OSLogger.cc
+++ os/posix/OSLogger.cc
@@ -26,7 +26,50 @@
 
 #include <qcc/OSLogger.h>
 
+#if defined (QCC_OS_ANDROID)
+
+#include <android/log.h>
+
+static void AndroidLogCB(DbgMsgType type, const char* module, const char* msg, void* context)
+{
+    int priority;
+    switch (type) {
+    case DBG_LOCAL_ERROR:
+    case DBG_REMOTE_ERROR:
+        priority = ANDROID_LOG_ERROR;
+        break;
+
+    case DBG_HIGH_LEVEL:
+        priority = ANDROID_LOG_WARN;
+        break;
+
+    case DBG_GEN_MESSAGE:
+        priority = ANDROID_LOG_INFO;
+        break;
+
+    case DBG_API_TRACE:
+        priority = ANDROID_LOG_DEBUG;
+        break;
+
+    default:
+    case DBG_REMOTE_DATA:
+    case DBG_LOCAL_DATA:
+        priority = ANDROID_LOG_VERBOSE;
+        break;
+    }
+    __android_log_write(priority, module, msg);
+}
+
+QCC_DbgMsgCallback QCC_GetOSLogger(bool useOSLog)
+{
+    return useOSLog ? AndroidLogCB : NULL;
+}
+
+#else // plain posix
+
 QCC_DbgMsgCallback QCC_GetOSLogger(bool useOSLog)
 {
     return NULL;
 }
+
+#endif
