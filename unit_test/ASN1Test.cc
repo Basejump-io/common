4cc681f6305fc8a803b205acba90c784741a3f6d
Praveen Bhamidipati <praveenb@quicinc.com>
2012-08-22 16:39:52 -0700

    Addressing Code-Review Comments from George
    For ASN1Test.cc
    a. Print QStatus returned by methods in the testcase failure message
    b. Avoid line breaks, where possible
    c. Prefer C-style multi-line comments instead of multiple single line '//',
       as per the style convention.

--- unit_test/ASN1Test.cc
+++ unit_test/ASN1Test.cc
@@ -15,14 +15,59 @@
  *    limitations under the License.
  ******************************************************************************/
 #include <gtest/gtest.h>
+
+#include <Status.h>
 #include <qcc/Util.h>
+
 #include <qcc/Crypto.h>
 
 using namespace qcc;
 
+/*
+ * Separate testcase for empty string "", because of the way linebreaks
+ * are added by Crypto_ASN1::EncodeBase64.
+ * In particular, for the empty string, no line breaks are added
+ * by Crypto_ASN1::EncodeBase64.
+ */
+TEST(ASN1Test, encode_and_decode_base64_empty_string) {
+    QStatus status = ER_FAIL;
+
+    String empty_string = String("");
+    String actual_encoded_base64;
+
+    status = Crypto_ASN1::EncodeBase64(empty_string, actual_encoded_base64);
+
+    EXPECT_EQ(ER_OK, status) <<
+    "The function EncodeBase64 was unable to encode the empty string \"" <<
+    empty_string.c_str() << "\" to Base64 format. The status returned was: " <<
+    QCC_StatusText(status);
+
+    if (ER_OK == status) {
+        // The Base64 encoding of empty string is empty string itself.
+        EXPECT_STREQ(empty_string.c_str(), actual_encoded_base64.c_str()) <<
+        "The empty string \"" << empty_string.c_str() << "\" was converted to "
+        "Base64 format. The result \"" << actual_encoded_base64.c_str() <<
+        "\" was NOT an empty string.";
+    }
+
+    String actual_decoded_string;
+
+    status = Crypto_ASN1::DecodeBase64(empty_string, actual_decoded_string);
+    EXPECT_EQ(ER_OK, status) <<
+    "The function DecodeBase64 was unable to decode the empty string \"" <<
+    empty_string.c_str() << "\". The status returned was: " <<
+    QCC_StatusText(status);
+
+    if (ER_OK == status) {
+        EXPECT_STREQ(empty_string.c_str(), actual_decoded_string.c_str()) <<
+        "The empty string \"" << empty_string.c_str() << "\" was decoded from "
+        "Base64 format. The result was NOT an empty string.";
+    }
+}
+
 // The following test vectors were taken from
 // RFC4648 - https://tools.ietf.org/html/rfc4648#section-10
-static const char* raw_string_literal_array[] = {
+static const char* raw_literal[] = {
     "f",
     "fo",
     "foo",
@@ -43,21 +88,24 @@ static const char* expected_base64_array[] = {
 TEST(ASN1Test, encode_base64) {
     QStatus status = ER_FAIL;
 
-    for (uint8_t i = 0; i < ArraySize(raw_string_literal_array); i++) {
-        String raw_string = String(raw_string_literal_array[i]);
+    for (uint8_t i = 0; i < ArraySize(raw_literal); i++) {
+        String raw_string = String(raw_literal[i]);
         String actual_encoded_base64;
 
         status = Crypto_ASN1::EncodeBase64(raw_string, actual_encoded_base64);
         EXPECT_EQ(ER_OK, status) <<
         "The function EncodeBase64 was unable to encode the string \"" <<
-        raw_string.c_str() << "\" to Base64 format.";
+        raw_string.c_str() << "\" to Base64 format. "
+        "The status returned was: " << QCC_StatusText(status);
 
         if (ER_OK == status) {
             String expected_base64 = String(expected_base64_array[i]);
-            // qcc:Crypto_ASN1::EncodeBase64 calls LineBreak (inline),
-            // which is a helper function for putting lines breaks
-            // at the appropriate location.
-            // Accordingly, we add '\n' to the expected value.
+            /*
+             * qcc:Crypto_ASN1::EncodeBase64 calls LineBreak (inline),
+             * which is a helper function for putting lines breaks
+             * at the appropriate location.
+             * Accordingly, we add '\n' to the expected value.
+             */
             expected_base64 = expected_base64.append('\n');
             EXPECT_STREQ(expected_base64.c_str(),
                          actual_encoded_base64.c_str()) <<
@@ -83,7 +131,7 @@ TEST(ASN1Test, decode_base64) {
         base64_string.c_str() << "\".";
 
         if (ER_OK == status) {
-            String expected_decoded_string = String(raw_string_literal_array[i]);
+            String expected_decoded_string = String(raw_literal[i]);
             EXPECT_STREQ(expected_decoded_string.c_str(),
                          actual_decoded_string.c_str()) <<
             "The string \"" << base64_string.c_str() << "\" was decoded from "
@@ -121,7 +169,8 @@ TEST(ASN1Test, decode_base64_negative_test) {
         "The function DecodeBase64 should have rejected the improperly "
         "formatted Base64 data \"" << improperly_encoded_base64.c_str() <<
         "\" of length " << improperly_encoded_base64.length() <<
-        ", which is not a multiple of 4.";
+        ", which is not a multiple of 4. The status returned was: " <<
+        QCC_StatusText(status);
 
         // 2. The number of pad '=' characters must not exceed 2
         improperly_encoded_base64 = actual_encoded_base64.append("====");
@@ -132,18 +181,28 @@ TEST(ASN1Test, decode_base64_negative_test) {
         "formatted Base64 data \"" << improperly_encoded_base64.c_str() <<
         "\", which has more than two pad characters.";
 
-        // 3. The Base64 encoded data must only contain characters from
-        //    'The Base 64 Alphabet' (65-character subset of US-ASCII)
-        //
-        //    The Alphabet used for Base64 is:
-        //    ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/
-        //
-        //    Remaining ASCII printable, non-whitespace, characters
+        /*
+         * 3. The Base64 encoded data must only contain characters from
+         *    'The Base 64 Alphabet' (65-character subset of US-ASCII)
+         *
+         *    The Alphabet used for Base64 is:
+         *    ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/
+         */
+
+        // Remaining ASCII printable, non-whitespace, characters
         String remaining_printable_ascii_characters =
             String("`~!@#$%^&*()-_[]{}\\|;:'\",<.>/?");
 
         for (size_t i = 0; i < remaining_printable_ascii_characters.length();
              i++) {
+            /*
+             * The length of the original string is 110.
+             * The length of the Base64 encoded string would be 144 (aprox)
+             * The number of remaining printable ascii characters is 30
+             * Hence, it is safe to insert a character at the 'i'th index
+             * into Base64 encoded string.
+             * 'i'th index is a crude random position to insert a character.
+             */
             improperly_encoded_base64 = actual_encoded_base64.insert(
                 i, &remaining_printable_ascii_characters[i]);
 
@@ -154,46 +213,9 @@ TEST(ASN1Test, decode_base64_negative_test) {
             "formatted Base64 data \"" << improperly_encoded_base64.c_str() <<
             "\", which contains the character " <<
             remaining_printable_ascii_characters[i] << ", that is not in "
-            "The Base 64 Alphabet.";
+            "The Base 64 Alphabet. The status returned was: " <<
+            QCC_StatusText(status);
         }
-    }
-}
-
-// Separate testcase for null string "", because of the way linebreaks
-// are added by Crypto_ASN1::EncodeBase64.
-// In particular, for the null string, no line breaks are added
-// by Crypto_ASN1::EncodeBase64.
-TEST(ASN1Test, encode_and_decode_base64_null_string) {
-    QStatus status = ER_FAIL;
-
-    String null_string = String("");
-    String actual_encoded_base64;
-
-    status = Crypto_ASN1::EncodeBase64(null_string, actual_encoded_base64);
-
-    EXPECT_EQ(ER_OK, status) <<
-    "The function EncodeBase64 was unable to encode the null string \"" <<
-    null_string.c_str() << "\" to Base64 format.";
-
-    if (ER_OK == status) {
-        // The Base64 encoding of null string is null string itself.
-        EXPECT_STREQ(null_string.c_str(), actual_encoded_base64.c_str()) <<
-        "The null string \"" << null_string.c_str() << "\" was converted to "
-        "Base64 format. The result \"" << actual_encoded_base64.c_str() <<
-        "\" was NOT a null string.";
-    }
-
-    String actual_decoded_string;
-
-    status = Crypto_ASN1::DecodeBase64(null_string, actual_decoded_string);
-    EXPECT_EQ(ER_OK, status) <<
-    "The function DecodeBase64 was unable to decode the null string \"" <<
-    null_string.c_str() << "\".";
-
-    if (ER_OK == status) {
-        EXPECT_STREQ(null_string.c_str(), actual_decoded_string.c_str()) <<
-        "The null string \"" << null_string.c_str() << "\" was decoded from "
-        "Base64 format. The result was NOT a null string.";
     }
 }
 
@@ -215,8 +237,7 @@ TEST(ASN1Test, encode_and_decode_base64_stress_test) {
             raw_data.append(quote_of_atticus_finch,
                             strlen(quote_of_atticus_finch));
         } else {
-            raw_data.append(quote_of_james_bond,
-                            strlen(quote_of_james_bond));
+            raw_data.append(quote_of_james_bond, strlen(quote_of_james_bond));
         }
 
         String actual_encoded_base64;
@@ -224,7 +245,8 @@ TEST(ASN1Test, encode_and_decode_base64_stress_test) {
         status = Crypto_ASN1::EncodeBase64(raw_data, actual_encoded_base64);
         EXPECT_EQ(ER_OK, status) <<
         "The function EncodeBase64 was unable to encode the string \"" <<
-        raw_data.c_str() << "\" to Base64 format.";
+        raw_data.c_str() << "\" to Base64 format. The status returned was: " <<
+        QCC_StatusText(status);
 
         if (ER_OK == status) {
             String actual_decoded_string;
@@ -232,7 +254,8 @@ TEST(ASN1Test, encode_and_decode_base64_stress_test) {
                                                actual_decoded_string);
             EXPECT_EQ(ER_OK, status) <<
             "The function DecodeBase64 was unable to decode the string \"" <<
-            actual_encoded_base64.c_str() << "\".";
+            actual_encoded_base64.c_str() << "\". The status returned was: " <<
+            QCC_StatusText(status);
 
             if (ER_OK == status) {
                 EXPECT_STREQ(raw_data.c_str(),
